a4e293f8878372c2b9e253695fee1100fd6ec9ad
Yehor <yehor12344321@gmail.com>
2024-01-19 19:08:36 +0200

    зберігає зміни

---
+++ index.js
@@ -0,0 +1,51 @@
+// const str1 = "text1"
+// const str2 = ""
+
+// if(str1.length > 0 && str2.length > 0){
+//     alert("Обидва поля заповнені");
+// }else{
+//     alert("Не всі поля заповнені");
+// }
+
+// const num1 = 5;
+// const num2 = 5;
+
+
+
+// if(num1 + num2 > 10){
+//     alert("Сума більша за 10")
+// }else if(num1 + num2 < 10 || num1 + num2 === 10){
+//     alert("Сума менша або дорівнює 10")
+// }
+
+// const str1 = "javaScript";
+
+// if(str1.includes("javaScript")){
+//     alert("Текст містить слово JavaScript");
+// }else{
+//     alert("Текст не містить слово JavaScript");
+// }
+
+// const num1 = 13
+
+// if(num1 > 10 && num1 < 20){
+//     alert("Число входить в діапазон від 10 до 20");
+// }else{
+//     alert("Число не входить в діапазон від 10 до 20");
+// }
+
+// const userName = "Yehor";
+
+// const email = "yehor@.gmail.com";
+
+// const password = "password1234";
+
+// if(userName.lenght > 3 && email.includes("@") && email.includes("@.") && password.length > 6){
+//     alert("Ви успішно авторизувалися!");
+// }else{
+//     alert("Помилка авторізації!");
+// }
+
+
+
+
